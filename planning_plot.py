4ca097b7fb92075242064a555c2ce29207aa377f
jocmom <jombi@web.de>
2018-06-18 15:40:00 +0200

    ADD: grid and path plots

--- planning_plot.py
+++ planning_plot.py
@@ -9,12 +9,12 @@ def plot_grid(grid):
     plt.ylabel('NORTH')
     plt.show()
     
-def plot_path(grid, path, start_ne, goal_ne):
+def plot_path(grid, path, start, goal):
     plt.imshow(grid, cmap='Greys', origin='lower')
     # For the purposes of the visual the east coordinate lay along
     # the x-axis and the north coordinates long the y-axis.
-    plt.plot(start_ne[1], start_ne[0], 'x')
-    plt.plot(goal_ne[1], goal_ne[0], 'x')
+    plt.plot(start[1], start[0], 'x')
+    plt.plot(goal[1], goal[0], 'x')
 
     pp = np.array(path)
     plt.plot(pp[:, 1], pp[:, 0], 'g')
